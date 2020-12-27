ac4d71c1c790ea7e7e459032bcf98a65e8fe9731
mshtang <32868278+mshtang@users.noreply.github.com>
2020-12-27 17:22:30 +0100

    first commit

---
+++ DocScanner.Core.Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using DocScanner.Core.Services;
+using OpenCvSharp;
+
+namespace DocScanner.Core.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagePath = @"..\..\..\..\Data\test1.jpeg";
+            var imageProc = new ImageProcessor(imagePath);
+
+            using var src = imageProc.GrayImage;
+            using var dst = new Mat();
+            src.CopyTo(dst);
+
+            var win = new Window("edges", WindowMode.FreeRatio, dst);
+            var trackbar = win.CreateTrackbar("thresh", 100, 256, thr =>
+            {
+                Cv2.Canny(src, dst, thr, thr * 2);
+                win.Image = dst;
+            });
+
+            while (true)
+            {
+                if (27 == (char)Cv2.WaitKey())
+                    break;
+            }
+
+            win.Dispose();
+        }
+
+    }
+}
---
+++ DocScanner.Core/Services/ImageProcessor.cs
@@ -0,0 +1,35 @@
+﻿using OpenCvSharp;
+using System.Collections.Generic;
+
+namespace DocScanner.Core.Services
+{
+    public class ImageProcessor
+    {
+        public List<Point> Boundary { get; set; }
+
+        public Mat OrigImage { get; set; }
+
+        public Mat GrayImage { get; set; }
+
+        private Mat _grayImage = new Mat();
+
+        public ImageProcessor()
+        {
+
+        }
+
+        public ImageProcessor(string imageFile)
+        {
+            OrigImage = new Mat(imageFile, ImreadModes.AnyColor);
+            Cv2.CvtColor(OrigImage, _grayImage, ColorConversionCodes.BGR2GRAY);
+            GrayImage = _grayImage;
+        }
+
+
+        public List<Point> GetOuterBoundary()
+        {
+            return null;
+        }
+
+    }
+}
